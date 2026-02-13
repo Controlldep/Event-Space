9de81ce362e42eb8fad14af5038c13cbffb17acf
Klopov Vladimir <faliingsky@mail.ru>
2026-02-13 11:11:39 +0400

    feat(exception): add exception filters and connected in main

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,41 @@ import {
   SwaggerDocumentOptions,
   SwaggerModule,
 } from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { ValidationPipe } from '@nestjs/common';
+import {
+  CustomHttpException,
+  DomainExceptionCode,
+} from './core/exceptions/domain.exceptions';
+import cookieParser from 'cookie-parser';
+import { CustomExceptionFilter } from './core/exceptions/exceptionts-filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.set('trust proxy');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      forbidNonWhitelisted: true,
+      disableErrorMessages: false,
+      exceptionFactory: (errors) => {
+        const details = errors.map((error) => ({
+          property: error.property,
+          constraints: error.constraints,
+        }));
+        throw new CustomHttpException(
+          DomainExceptionCode.BAD_REQUEST,
+          'Validation failed',
+          details,
+        );
+      },
+    }),
+  );
+
+  app.use(cookieParser());
+  app.useGlobalFilters(new CustomExceptionFilter());
 
   const config = new DocumentBuilder()
     .setTitle('tickets example')
@@ -18,6 +50,7 @@ async function bootstrap() {
   const options: SwaggerDocumentOptions = {
     operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
   };
+
   const documentFactory = () =>
     SwaggerModule.createDocument(app, config, options);
   SwaggerModule.setup('api', app, documentFactory);
