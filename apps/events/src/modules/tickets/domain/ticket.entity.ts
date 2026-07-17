20add99a10bd80a565ace90aa74b757c9f6b0cae
Klopov Vladimir <faliingsky@mail.ru>
2026-07-17 20:00:51 +0400

    feat(ticket): update ticket.entity

--- apps/events/src/modules/tickets/domain/ticket.entity.ts
+++ apps/events/src/modules/tickets/domain/ticket.entity.ts
@@ -2,6 +2,12 @@ import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGenerat
 import type { EventEntity } from '../../events/domain/event.entity';
 import { CreateTicketDto } from './input-dto/create-ticket.dto';
 
+export enum TicketStatus {
+  RESERVED = 'reserved',
+  PURCHASED = 'purchased',
+  CANCELLED = 'cancelled',
+}
+
 @Entity('tickets')
 export class TicketEntity {
   @PrimaryGeneratedColumn('uuid')
@@ -19,6 +25,12 @@ export class TicketEntity {
   @UpdateDateColumn()
   updatedAt: Date;
 
+  @Column({ type: 'enum', enum: TicketStatus, default: TicketStatus.RESERVED })
+  status: TicketStatus;
+
+  @Column({ type: 'timestamp', nullable: true })
+  reservedAt: Date;
+
   @ManyToOne('EventEntity', (event: EventEntity) => event.tickets)
   @JoinColumn({ name: 'eventId' })
   event: EventEntity;
@@ -26,6 +38,7 @@ export class TicketEntity {
   static createInstance(dto: CreateTicketDto): TicketEntity {
     const ticket: TicketEntity = new this();
     ticket.userId = dto.userId;
+    ticket.reservedAt = new Date();
     ticket.eventId = dto.eventId;
 
     return ticket;
