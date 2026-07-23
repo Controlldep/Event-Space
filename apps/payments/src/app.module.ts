209e31eec01fad65a557bc88c6f2b9f5cad45d3d
Klopov Vladimir <faliingsky@mail.ru>
2026-07-23 20:36:02 +0400

    feat(payments): add usecase in providers

--- apps/payments/src/app.module.ts
+++ apps/payments/src/app.module.ts
@@ -6,10 +6,20 @@ import { ScheduleModule } from '@nestjs/schedule';
 import { LoggerModule, RequestContextMiddleware } from '@app/logger';
 import databaseConfig from './core/config/database.config';
 import { envSchema } from './core/config/env.validation';
+import { PaymentsJwtStrategy } from './guard/payments.strategy';
+import { PassportModule } from '@nestjs/passport';
+import { PaymentsJwtGuard } from './guard/payments-jwt.guard';
+import { PaymentsTcpController } from './api/payments.tcp.controller';
+import { CreateCheckoutSessionUseCase } from './application/use-case/command/create-checkoutSession.use-case';
+import { StripeWebhookUseCase } from './application/use-case/command/stripe-webhook.use-case';
+import { GetPaymentHistoryHandler } from './application/use-case/query/get-payment-history';
+import { RefundPaymentUseCase } from './application/use-case/command/refund-payment.use-case';
+import { StripeService } from './application/stripe.service';
 
 @Module({
   imports: [
     LoggerModule,
+    PassportModule,
     ScheduleModule.forRoot(),
     ConfigModule.forRoot({
       isGlobal: true,
@@ -30,8 +40,16 @@ import { envSchema } from './core/config/env.validation';
       useFactory: (configService: ConfigService) => configService.get<TypeOrmModuleOptions>('database')!,
     }),
   ],
-  controllers: [],
-  providers: [],
+  controllers: [PaymentsTcpController],
+  providers: [
+    PaymentsJwtStrategy,
+    PaymentsJwtGuard,
+    CreateCheckoutSessionUseCase,
+    StripeWebhookUseCase,
+    GetPaymentHistoryHandler,
+    RefundPaymentUseCase,
+    StripeService,
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
